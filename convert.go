b6b80a6bd971506a1367d04741b6f120efb219a9
alex-x-crypto <67682756+alex-x-crypto@users.noreply.github.com>
2023-06-15 16:44:25 +0800

    add FromEther

--- convert.go
+++ convert.go
@@ -9,7 +9,20 @@ func ToGwei(wei *big.Int) string {
 }
 
 func ToEther(wei *big.Int) string {
-	g := new(big.Float).SetUint64(wei.Uint64())
-	g.Quo(g, big.NewFloat(1e18))
-	return g.String()
+	e := new(big.Float).SetUint64(wei.Uint64())
+	e.Quo(e, big.NewFloat(1e18))
+	return e.String()
+}
+
+func FromEther(ether string) (*big.Int, bool) {
+	e, success := new(big.Float).SetString(ether)
+	if !success {
+		return nil, success
+	}
+	e.Mul(e, big.NewFloat(1e18))
+	e2, a := e.Int(nil)
+	if a != big.Exact {
+		return nil, false
+	}
+	return e2, true
 }
